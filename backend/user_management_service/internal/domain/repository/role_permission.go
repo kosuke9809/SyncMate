3c3e917ff132712435be1c63d67daf5734de5a95
kosuke9809 <uht0912k@outlook.com>
2024-05-02 23:06:52 +0900

    add: role&permission

--- backend/user_management_service/internal/domain/repository/role_permission.go
+++ backend/user_management_service/internal/domain/repository/role_permission.go
@@ -3,7 +3,7 @@ package repository
 import "github.com/kosuke9809/SyncMate/internal/domain/model"
 
 type IRolePermissionRepository interface {
-	HasPermission(roleID, rolePermission uint) (bool, error)
+	HasPermission(roleID, permissionID uint) (bool, error)
 	AddPermissionToRole(roleID uint, permissionID uint) error
 	RemovePermissionFromRole(roleID uint, permissionID uint) error
 	GetRolePermissions(roleID uint) ([]*model.Permission, error)
--- backend/user_management_service/internal/domain/service/role.go
+++ backend/user_management_service/internal/domain/service/role.go
@@ -13,25 +13,25 @@ type IRoleService interface {
 	DeleteRole(roleID uint) error
 	FindByID(roleID uint) (*model.Role, error)
 	FindByName(roleName string) (*model.Role, error)
-	GetAll() ([]*model.Role, error)
+	GetAllRoles() ([]*model.Role, error)
 }
 
 type roleService struct {
-	rp repository.IRoleRepository
+	rr repository.IRoleRepository
 }
 
-func NewRoleService(rp repository.IRoleRepository) IRoleService {
-	return &roleService{rp}
+func NewRoleService(rr repository.IRoleRepository) IRoleService {
+	return &roleService{rr}
 }
 
 func (rs *roleService) CreateRole(roleName string) (*model.Role, error) {
-	if _, err := rs.rp.FindByName(roleName); err == nil {
+	if _, err := rs.rr.FindByName(roleName); err == nil {
 		return nil, errors.New("role already exists")
 	}
 	role := &model.Role{
 		RoleName: roleName,
 	}
-	newRole, err := rs.rp.Create(role)
+	newRole, err := rs.rr.Create(role)
 	if err != nil {
 		return nil, err
 	}
@@ -39,32 +39,32 @@ func (rs *roleService) CreateRole(roleName string) (*model.Role, error) {
 }
 
 func (rs *roleService) UpdateRole(roleID uint, roleName string) (*model.Role, error) {
-	role, err := rs.rp.FindByID(roleID)
+	role, err := rs.rr.FindByID(roleID)
 	if err != nil {
 		return nil, errors.New("role not found")
 	}
 	role.RoleName = roleName
-	return rs.rp.Update(role)
+	return rs.rr.Update(role)
 }
 
 func (rs *roleService) DeleteRole(roleID uint) error {
-	if _, err := rs.rp.FindByID(roleID); err != nil {
+	if _, err := rs.rr.FindByID(roleID); err != nil {
 		return errors.New("role not found")
 	}
-	if err := rs.rp.Delete(roleID); err != nil {
+	if err := rs.rr.Delete(roleID); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (rs *roleService) FindByID(roleID uint) (*model.Role, error) {
-	return rs.rp.FindByID(roleID)
+	return rs.rr.FindByID(roleID)
 }
 
 func (rs *roleService) FindByName(roleName string) (*model.Role, error) {
-	return rs.rp.FindByName(roleName)
+	return rs.rr.FindByName(roleName)
 }
 
-func (rs *roleService) GetAll() ([]*model.Role, error) {
-	return rs.rp.GetAll()
+func (rs *roleService) GetAllRoles() ([]*model.Role, error) {
+	return rs.rr.GetAll()
 }
